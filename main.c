44d30be7e46022d518a0080103d3d17a49786078
Sebastian Moosbauer <sebastian.moosbauer@thu.de>
2025-05-14 23:10:13 +0200

    remove project and add standalone main.c

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 
 #include <xc.h>
 
+uint16_t test;
+
 void __init(void);
 
 static unsigned char led_mask = 0b00111100;
@@ -15,19 +17,25 @@ void main(void) {
     __init();
 
     while (1) {
+       // time wasting loop for ?? ms
        for(int i = 0; i < 10000; ++i){
            Nop();
        }
+       
+       // toggle LEDs
        LATB ^= led_mask;
+       // what's happening here? (We'll find out at the end of this lab ;) )
+       unsigned char* bla = (unsigned char*)0xF8A;
+       *bla = 0xFF;
     }
 
     return;
 }
 
 void __init(void) {
-    OSCCON = 0x50;
+    OSCCON = 0x50; // Fosc = 4MHz
     
-    ANSELB = 0;
+    ANSELB = 0x00;
     TRISB &= 0b11000011;
     LATB   = 0b00010100;
 }
